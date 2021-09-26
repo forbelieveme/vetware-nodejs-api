34b33dc1bedecd0f3b86c154eda030b56d955027
forbelieveme <haner.avella@protonmail.com>
2021-09-25 19:18:02 -0500

    first commit

---
+++ database.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const { MongoClient } = require('mongodb');
+const uri = process.env.DB_URI;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+  try {
+    await client.connect();
+
+    console.log('Connected correctly to server');
+  } catch (err) {
+    console.log(err.stack);
+  } finally {
+    await client.close();
+  }
+}
+
+run().catch(console.dir);
