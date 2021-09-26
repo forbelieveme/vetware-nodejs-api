94c7a0e7f922ca79e2bf9740a2b9847c16d22153
forbelieveme <haner.avella@protonmail.com>
2021-09-26 16:57:55 -0500

    Crea la ruta del metodo post para la insercion de una nueva mascota

---
+++ routes/pets.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+const { newPet } = require('../controllers/pets.controller');
+
+// router.route('/api/pets/:email/:pet_id').get(getUserPet);
+// router.route('/api/pets').get(getAllUserPets);
+router.route('/api/pets').post(newPet);
+
+//utilities
+// router.route('/api/pets/breeds').get(getAllBreeds);
+
+
+
+module.exports = router;
