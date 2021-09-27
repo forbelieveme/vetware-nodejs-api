6c6e30b4cc07cf11fbf23da3b8a20558db3aaa4b
forbelieveme <haner.avella@protonmail.com>
2021-09-26 19:07:38 -0500

    Inserta el enrutamiento de los servicios en la aplicacion

--- app.js
+++ app.js
@@ -12,5 +12,6 @@ app.use(express.json());
 // routes
 app.use('/', require('./routes/users'));
 app.use('/', require('./routes/pets'));
+app.use('/', require('./routes/services'));
 
 module.exports = app;
