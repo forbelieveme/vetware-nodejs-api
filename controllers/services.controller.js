eb21a991cd92c912c15d2e9c6ae30d3e28d196b6
forbelieveme <haner.avella@protonmail.com>
2021-09-26 19:09:09 -0500

    Genera el controlador de los servicios con los metodos de crear un servicio y obtener todos los servicios

---
+++ controllers/services.controller.js
@@ -0,0 +1,47 @@
+require('dotenv').config();
+const client = require('../database');
+
+const servicesController = {};
+
+servicesController.getServices = async (req, res) => {
+    try {
+        await client.connect();
+
+        const database = client.db(process.env.DB_NAME);
+        const services = database.collection('services');
+
+        const allServices = await services.find().toArray();
+
+        res.json(allServices);
+    } catch (err) {
+        res.json(err.stack);
+    } finally {
+        await client.close();
+    }
+}
+
+servicesController.newService = async (req, res) => {
+    const { name, duration } = req.body;
+    try {
+        await client.connect();
+
+        const database = client.db(process.env.DB_NAME);
+        const services = database.collection('services');
+
+        // create a document to insert
+        const service = {
+            name,
+            duration
+        };
+
+        const result = await services.insertOne(service);
+
+        res.json({ message: `A document was inserted with the _id: ${result.insertedId}` });
+    } catch (err) {
+        console.log(err.stack);
+    } finally {
+        await client.close();
+    }
+}
+
+module.exports = servicesController;
