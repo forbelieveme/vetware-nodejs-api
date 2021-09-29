d016b4a1f8a1b9280db1b157f3dbb0ded646132a
forbelieveme <haner.avella@protonmail.com>
2021-09-29 17:30:27 -0500

    Inserta la ruta para la peticion de leer todas las mascotas por usuario

--- routes/pets.js
+++ routes/pets.js
@@ -1,15 +1,13 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const { newPet } = require('../controllers/pets.controller');
+const { newPet, getUserPets } = require("../controllers/pets.controller");
 
 // router.route('/api/pets/:email/:pet_id').get(getUserPet);
-// router.route('/api/pets').get(getAllUserPets);
-router.route('/api/pets/newPet').post(newPet);
+router.route("/api/pets/getUserPets").post(getUserPets);
+router.route("/api/pets/newPet").post(newPet);
 
 //utilities
 // router.route('/api/pets/breeds').get(getAllBreeds);
-
-
 
 module.exports = router;
