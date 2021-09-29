8520e73ef987a121933213ce5718fb9647216dae
forbelieveme <haner.avella@protonmail.com>
2021-09-29 16:01:09 -0500

    Anade el id del usuario en el schema de la mascota

--- controllers/pets.controller.js
+++ controllers/pets.controller.js
@@ -1,41 +1,36 @@
-require('dotenv').config();
-const client = require('../database');
+require("dotenv").config();
+const client = require("../database");
 
 const petsController = {};
 
 petsController.newPet = async (req, res) => {
-    // const { owner_email, name, date_birth, species, breed } = req.body;
-    const { owner_email, name, species, breed } = req.body;
-    try {
-        await client.connect();
+  // const { owner_email, name, date_birth, species, breed } = req.body;
+  const { owner_id, date_birth, name, species, breed } = req.body;
+  try {
+    await client.connect();
 
-        const database = client.db(process.env.DB_NAME);
-        const pets = database.collection('pets');
-        const users = database.collection('users');
+    const database = client.db(process.env.DB_NAME);
+    const pets = database.collection("pets");
 
-        // // create a document to insert
-        const pet = {
-            name,
-            // date_birth,
-            species,
-            breed,
-            history: []
-        };
+    // // create a document to insert
+    const pet = {
+      name,
+      owner_id,
+      date_birth,
+      species,
+      breed,
+    };
 
-        const result_1 = await pets.insertOne(pet);
+    const result_1 = await pets.insertOne(pet);
 
-        const query = { email: owner_email }
-        const updateOwner = {
-            $push: { pets: result_1.insertedId }
-        }
-
-        const result_2 = await users.updateOne(query, updateOwner);
-        res.json({ message: `A document was inserted with the _id: ${result_1.insertedId}` });
-    } catch (err) {
-        console.log(err.stack);
-    } finally {
-        await client.close();
-    }
+    res.json({
+      message: `A new pet was created with the _id: ${result_1.insertedId}`,
+    });
+  } catch (err) {
+    console.log(err.stack);
+  } finally {
+    await client.close();
+  }
 };
 
 module.exports = petsController;
