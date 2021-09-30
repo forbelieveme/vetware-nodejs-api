5863abbda982c7a2745d743aa030bf1bca045568
forbelieveme <haner.avella@protonmail.com>
2021-09-30 13:45:43 -0500

    Agrega el enrutamiento para el metodo de actualizacion de estado de las citas

--- routes/appointments.js
+++ routes/appointments.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const { newAppointment } = require('../controllers/appointments.controller');
+const { newAppointment , updateAppointmentStatus} = require('../controllers/appointments.controller');
 
 router.route('/api/appointments/newAppointment').post(newAppointment);
+router.route('/api/appointments/updateAppointmentStatus').put(updateAppointmentStatus);
 
 module.exports = router;
