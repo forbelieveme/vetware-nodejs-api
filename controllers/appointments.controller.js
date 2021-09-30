ddd637023cf3c88ee7133b8d5dd3a0d84fe5762b
forbelieveme <haner.avella@protonmail.com>
2021-09-30 13:46:18 -0500

    Anade el metodo que realiza la actualizacion del estado de la cita

--- controllers/appointments.controller.js
+++ controllers/appointments.controller.js
@@ -1,33 +1,65 @@
-require('dotenv').config();
-const client = require('../database');
+require("dotenv").config();
+const { ObjectId } = require("mongodb");
+const client = require("../database");
 
 const appointmentsController = {};
 
+appointmentsController.updateAppointmentStatus = async (req, res) => {
+  const { appointment_id, status } = req.body;
+  try {
+    await client.connect();
+
+    const database = client.db(process.env.DB_NAME);
+    const appointments = database.collection("appointments");
+
+    const query = { _id: ObjectId(appointment_id) };
+
+    const update = {
+      $set: {
+        status,
+      },
+    };
+    // create a document to insert
+
+    const result_1 = await appointments.updateOne(query, update);
+
+    res.json({
+      message: `A new appointment was created with the _id: ${result_1.insertedId}`,
+    });
+  } catch (err) {
+    console.log(err.stack);
+  } finally {
+    await client.close();
+  }
+};
+
 appointmentsController.newAppointment = async (req, res) => {
-    const { pet_id, service_id, start_date, end_date, status } = req.body;
-    try {
-        await client.connect();
+  const { pet_id, service_id, appointment_date, end_date, status } = req.body;
+  try {
+    await client.connect();
 
-        const database = client.db(process.env.DB_NAME);
-        const appointments = database.collection('appointments');
+    const database = client.db(process.env.DB_NAME);
+    const appointments = database.collection("appointments");
 
-        // create a document to insert
-        const appointment = {
-            pet_id,
-            service_id,
-            start_date,
-            end_date,
-            status
-        };
+    // create a document to insert
+    const appointment = {
+      pet_id,
+      service_id,
+      appointment_date,
+      creation_date: new Date(Date.now()),
+      status,
+    };
 
-        const result_1 = await appointments.insertOne(appointment);
+    const result_1 = await appointments.insertOne(appointment);
 
-        res.json({ message: `A new appointment was created with the _id: ${result_1.insertedId}` });
-    } catch (err) {
-        console.log(err.stack);
-    } finally {
-        await client.close();
-    }
-}
+    res.json({
+      message: `A new appointment was created with the _id: ${result_1.insertedId}`,
+    });
+  } catch (err) {
+    console.log(err.stack);
+  } finally {
+    await client.close();
+  }
+};
 
 module.exports = appointmentsController;
